b26659d975d99e1ed5afc8c5553c96b59cedc860
Fullganne <firewolf148@gmail.com>
2023-12-01 16:00:11 +0700

    make in vsc

--- Program.cs
+++ Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JenkinsC_Demo
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello guys");
-            Console.WriteLine("Welcome you to Jenkins World!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
